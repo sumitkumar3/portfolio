fd361fea2d40769bbb9fc7f3298c71345279b656
sumitkumar3 <sumit.kumar3@go-mmt.com>
2019-08-01 13:12:10 +0530

    bootstrap added and jobs view

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    body = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='images/')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogs.apps.BlogsConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,10 +75,20 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD':'mih@1234',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
